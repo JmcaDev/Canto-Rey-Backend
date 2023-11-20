964692dda36dda13a57c7adcf8d2b2e17fcd2a47
JmcaDev <jmcavzla@gmail.com>
2023-11-20 12:26:50 -0400

    BD configurada

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import conectarDb from "./config/db.js"
+
+const app = express();
+dotenv.config();
+
+conectarDb()
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+    console.log(`servidor oyendo en el puerto ${PORT}`)
+})
